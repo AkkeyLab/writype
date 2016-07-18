ce94b2aa108c1371ff787d33ed4c82011d4dc70b
金築良磨 <dominor16@gmail.com>
2016-07-18 23:22:32 +0900

    Xcodeファイルを追加した

---
+++ writype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  writype
+//
+//  Created by 金築良磨 on 2016/07/18.
+//  Copyright © 2016年 AkkeyLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
