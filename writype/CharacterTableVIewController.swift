ca33b0989581f5cc163222dab8b6274ffe7907ef
金築良磨 <dominor16@gmail.com>
2016-08-12 11:42:58 +0900

    フォント検索、フォント作成、レポート作成のviewContollerクラスを作成

---
+++ writype/CharacterTableVIewController.swift
@@ -0,0 +1,40 @@
+//
+//  CharacterTableVIewController.swift
+//  writype
+//
+//  Created by 金築良磨 on 2016/07/25.
+//  Copyright © 2016年 AkkeyLab. All rights reserved.
+//
+
+class CharacterTableVIewController: UITableViewController {
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        // #warning Incomplete implementation, return the number of sections
+        
+        return 1
+        
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // #warning Incomplete implementation, return the number of rows
+        
+        return 10
+        
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+        
+    {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        // Configure the cell...
+        
+        cell.textLabel?.text = indexPath.row.description
+        
+        return cell
+        
+    }
+}
