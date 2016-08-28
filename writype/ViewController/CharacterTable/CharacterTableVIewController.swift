e61f73b6802a767bcd9e175ea665a78eccab0221
AKIO <maizima44ww@gmail.com>
2016-08-28 15:58:36 +0900

    デザイン仮確定

--- writype/ViewController/CharacterTable/CharacterTableVIewController.swift
+++ writype/ViewController/CharacterTable/CharacterTableVIewController.swift
@@ -6,35 +6,37 @@
 //  Copyright © 2016年 AkkeyLab. All rights reserved.
 //
 
+import UIKit
+
 class CharacterTableVIewController: UITableViewController {
-    
+
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        
+
         // #warning Incomplete implementation, return the number of sections
-        
+
         return 1
-        
+
     }
-    
+
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+
         // #warning Incomplete implementation, return the number of rows
-        
+
         return 10
-        
+
     }
-    
+
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-        
+
     {
-        
+
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-        
+
         // Configure the cell...
-        
+
         cell.textLabel?.text = indexPath.row.description
-        
+
         return cell
-        
+
     }
 }
