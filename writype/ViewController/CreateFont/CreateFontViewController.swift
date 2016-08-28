a4e5398f7740c92f9cff095d16013101c3122d2b
AKIO <maizima44ww@gmail.com>
2016-08-28 14:37:19 +0900

    ファイル移動と、フォント作成画面のレイアウト下地作成。

--- writype/ViewController/CreateFont/CreateFontViewController.swift
+++ writype/ViewController/CreateFont/CreateFontViewController.swift
@@ -14,22 +14,22 @@ class CreateFontViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 
 }
